e4900324245e8d77b274a92cc1149bc435bd40de
hashalite <hashalite@gmail.com>
2022-12-16 22:50:32 -0500

    - Made more strings translatable - Updated translations

--- src/main/java/net/xolt/freecam/ConfigScreen.java
+++ src/main/java/net/xolt/freecam/ConfigScreen.java
@@ -42,8 +42,8 @@ public class ConfigScreen extends Screen {
     
     OptionInstance<FreecamConfig.FlightMode> flightMode = new OptionInstance<FreecamConfig.FlightMode>(
         "text.freecam.configScreen.option.flightMode",
-        (value2) -> Tooltip.create(Component.literal("The type of flight used by freecam.")),
-        (unused, option) -> Component.literal(option.getKey()),
+        (value2) -> Tooltip.create(Component.translatable("text.freecam.configScreen.option.flightMode.tooltip")),
+        (unused, option) -> Component.translatable(option.getKey()),
         new OptionInstance.Enum<>(Arrays.asList(FreecamConfig.FlightMode.values()), Codec.INT.xmap(FreecamConfig.FlightMode::byId, FreecamConfig.FlightMode::getId)),
         (FreecamConfig.FlightMode)FreecamConfig.FLIGHT_MODE.get(),
         (newValue) -> FreecamConfig.FLIGHT_MODE.set(newValue)
@@ -52,8 +52,8 @@ public class ConfigScreen extends Screen {
 
     OptionInstance<FreecamConfig.InteractionMode> interactionMode = new OptionInstance<FreecamConfig.InteractionMode>(
         "text.freecam.configScreen.option.interactionMode",
-        (value) -> Tooltip.create(Component.literal("The source of block/entity interactions.")),
-        (unused, option) -> Component.literal(option.getKey()),
+        (value) -> Tooltip.create(Component.translatable("text.freecam.configScreen.option.interactionMode.tooltip")),
+        (unused, option) -> Component.translatable(option.getKey()),
         new OptionInstance.Enum<>(Arrays.asList(FreecamConfig.InteractionMode.values()), Codec.INT.xmap(FreecamConfig.InteractionMode::byId, FreecamConfig.InteractionMode::getId)),
         (FreecamConfig.InteractionMode)FreecamConfig.INTERACTION_MODE.get(),
         (newValue) -> FreecamConfig.INTERACTION_MODE.set(newValue)
@@ -62,8 +62,8 @@ public class ConfigScreen extends Screen {
 
     OptionInstance<Double> horizontalSpeed = new OptionInstance<>(
         "text.freecam.configScreen.option.horizontalSpeed",
-        (value) -> Tooltip.create(Component.literal("The horizontal speed of freecam.")),
-        (unused, option) -> Component.literal("Horizontal Speed: " + FreecamConfig.HORIZONTAL_SPEED.get()),
+        (value) -> Tooltip.create(Component.translatable("text.freecam.configScreen.option.horizontalSpeed.tooltip")),
+        (unused, option) -> Component.translatable("text.freecam.configScreen.option.horizontalSpeed").append(": " + FreecamConfig.HORIZONTAL_SPEED.get()),
         OptionInstance.UnitDouble.INSTANCE,
         FreecamConfig.HORIZONTAL_SPEED.get() / 10,
         (value) -> {
@@ -75,8 +75,8 @@ public class ConfigScreen extends Screen {
 
     OptionInstance<Double> verticalSpeed = new OptionInstance<>(
         "text.freecam.configScreen.option.verticalSpeed",
-        (value) -> Tooltip.create(Component.literal("The vertical speed of freecam.")),
-        (unused, option) -> Component.literal("Vertical Speed: " + FreecamConfig.VERTICAL_SPEED.get()),
+        (value) -> Tooltip.create(Component.translatable("text.freecam.configScreen.option.verticalSpeed.tooltip")),
+        (unused, option) -> Component.translatable("text.freecam.configScreen.option.verticalSpeed").append(": " + FreecamConfig.VERTICAL_SPEED.get()),
         OptionInstance.UnitDouble.INSTANCE,
         FreecamConfig.VERTICAL_SPEED.get() / 10,
         (value) -> {
@@ -88,7 +88,7 @@ public class ConfigScreen extends Screen {
 
     OptionInstance<Boolean> noClip = OptionInstance.createBoolean(
         "text.freecam.configScreen.option.noClip",
-        (value) -> Tooltip.create(Component.literal("Whether you can travel through blocks in freecam.")),
+        (value) -> Tooltip.create(Component.translatable("text.freecam.configScreen.option.noClip.tooltip")),
         FreecamConfig.NO_CLIP.get(),
         (value) -> FreecamConfig.NO_CLIP.set(value)
     );
@@ -96,7 +96,7 @@ public class ConfigScreen extends Screen {
 
     OptionInstance<Boolean> disableOnDamage = OptionInstance.createBoolean(
         "text.freecam.configScreen.option.disableOnDamage",
-        (value) -> Tooltip.create(Component.literal("Disables freecam when damage is received.")),
+        (value) -> Tooltip.create(Component.translatable("text.freecam.configScreen.option.disableOnDamage.tooltip")),
         FreecamConfig.DISABLE_ON_DAMAGE.get(),
         (value) -> FreecamConfig.DISABLE_ON_DAMAGE.set(value)
     );
@@ -104,7 +104,7 @@ public class ConfigScreen extends Screen {
 
     OptionInstance<Boolean> allowInteract = OptionInstance.createBoolean(
         "text.freecam.configScreen.option.allowInteract",
-        (value) -> Tooltip.create(Component.literal("Whether you can interact with blocks/entities in freecam.\n\u00A7cWARNING: Multiplayer usage not advised.")),
+        (value) -> Tooltip.create(Component.translatable("text.freecam.configScreen.option.allowInteract.tooltip")),
         FreecamConfig.ALLOW_INTERACT.get(),
         (value) -> FreecamConfig.ALLOW_INTERACT.set(value)
     );
@@ -112,7 +112,7 @@ public class ConfigScreen extends Screen {
 
     OptionInstance<Boolean> freezePlayer = OptionInstance.createBoolean(
         "text.freecam.configScreen.option.freezePlayer",
-        (value) -> Tooltip.create(Component.literal("Prevents player movement while freecam is active.\n\u00A7cWARNING: Multiplayer usage not advised.")),
+        (value) -> Tooltip.create(Component.translatable("text.freecam.configScreen.option.freezePlayer.tooltip")),
         FreecamConfig.FREEZE_PLAYER.get(),
         (value) -> FreecamConfig.FREEZE_PLAYER.set(value)
     );
@@ -120,7 +120,7 @@ public class ConfigScreen extends Screen {
 
     OptionInstance<Boolean> showPlayer = OptionInstance.createBoolean(
         "text.freecam.configScreen.option.showPlayer",
-        (value) -> Tooltip.create(Component.literal("Shows your player in its original position.")),
+        (value) -> Tooltip.create(Component.translatable("text.freecam.configScreen.option.showPlayer.tooltip")),
         FreecamConfig.SHOW_PLAYER.get(),
         (value) -> FreecamConfig.SHOW_PLAYER.set(value)
     );
@@ -128,7 +128,7 @@ public class ConfigScreen extends Screen {
 
     OptionInstance<Boolean> showHand = OptionInstance.createBoolean(
         "text.freecam.configScreen.option.showHand",
-        (value) -> Tooltip.create(Component.literal("Whether you can see your hand in freecam.")),
+        (value) -> Tooltip.create(Component.translatable("text.freecam.configScreen.option.showHand.tooltip")),
         FreecamConfig.SHOW_HAND.get(),
         (value) -> FreecamConfig.SHOW_HAND.set(value)
     );
@@ -136,19 +136,19 @@ public class ConfigScreen extends Screen {
 
     OptionInstance<Boolean> notifyFreecam = OptionInstance.createBoolean(
         "text.freecam.configScreen.option.notifyFreecam",
-        (value) -> Tooltip.create(Component.literal("Notifies you when entering/exiting freecam.")),
+        (value) -> Tooltip.create(Component.translatable("text.freecam.configScreen.option.notifyFreecam.tooltip")),
         FreecamConfig.NOTIFY_FREECAM.get(),
         (value) -> FreecamConfig.NOTIFY_FREECAM.set(value)
     );
     this.optionsList.addBig(notifyFreecam);
 
-    OptionInstance<Boolean> notifyPersistent = OptionInstance.createBoolean(
-        "text.freecam.configScreen.option.notifyPersistent",
-        (value) -> Tooltip.create(Component.literal("Notifies you when entering/exiting tripod cameras.")),
-        FreecamConfig.NOTIFY_PERSISTENT.get(),
-        (value) -> FreecamConfig.NOTIFY_PERSISTENT.set(value)
+    OptionInstance<Boolean> notifyTripod = OptionInstance.createBoolean(
+        "text.freecam.configScreen.option.notifyTripod",
+        (value) -> Tooltip.create(Component.translatable("text.freecam.configScreen.option.notifyTripod.tooltip")),
+        FreecamConfig.NOTIFY_TRIPOD.get(),
+        (value) -> FreecamConfig.NOTIFY_TRIPOD.set(value)
     );
-    this.optionsList.addBig(notifyPersistent);
+    this.optionsList.addBig(notifyTripod);
 
     this.addWidget(optionsList);
 
